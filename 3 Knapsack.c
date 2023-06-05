10670208ca1baf30495b079af1770276812bec99
Khadeer shaik <122957297+ikhadeer07@users.noreply.github.com>
2023-06-05 20:03:29 +0530

    Update 3 Knapsack.c

--- 3 Knapsack.c
+++ 3 Knapsack.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct Item
 {
     int weight;
     int value;
 };
-
 int cmpfunc(const void* a, const void* b)
  {
     struct Item* item1 = (struct Item*) a;
@@ -20,33 +18,38 @@ int cmpfunc(const void* a, const void* b)
     else
         return 0;
 }
-
-double fractionalKnapsack(int W, struct Item* items, int n) {
+double fractionalKnapsack(int W, struct Item* items, int n) 
+{
     qsort(items, n, sizeof(struct Item), cmpfunc);
     int i;
     double totalValue = 0.0;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         if (W == 0)
             return totalValue;
-        if (items[i].weight <= W) {
+        if (items[i].weight <= W)
+        {
             totalValue += items[i].value;
             W -= items[i].weight;
-        } else {
+        }
+        else 
+        {
             totalValue += items[i].value * ((double)W / items[i].weight);
             W = 0;
         }
     }
     return totalValue;
 }
-
-int main() {
+int main() 
+{
     int n, i, W;
     printf("Enter the number of items: ");
     scanf("%d", &n);
     printf("Enter the maximum capacity of the knapsack: ");
     scanf("%d", &W);
     struct Item items[n];
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         printf("Enter the weight and value of item %d: ", i + 1);
         scanf("%d %d", &items[i].weight, &items[i].value);
     }
