a46b0569a9c8d6e36fe81626d4fa759b8fa61f4e
Khadeer shaik <ikhadeer07@gmail.com>
2023-06-20 11:16:40 +0530

    Add files via upload

---
+++ 7a pa.c
@@ -0,0 +1,64 @@
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define V 5
+
+int minKey(int key[], bool mstSet[]) {
+    int min = INT_MAX;
+    int min_index;
+    int v;
+    for (v = 0; v < V; v++) {
+        if (mstSet[v] == false && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+int printMST(int parent[], int graph[V][V]) {
+    printf("Edge\tWeight\n");
+    int i;
+    for (i = 1; i < V; i++) {
+        printf("%d-%d\t%d\n", parent[i], i, graph[i][parent[i]]);
+    }
+    return 0;
+}
+
+void primMST(int graph[V][V]) {
+    int parent[V];
+    int key[V];
+    bool mstSet[V];
+    int i;
+    for (i = 0; i < V; i++) {
+        key[i] = INT_MAX;
+        mstSet[i] = false;
+    }
+    key[0] = 0;
+    parent[0] = -1;
+    int count;
+    for (count = 0; count < V - 1; count++) {
+        int u = minKey(key, mstSet);
+        mstSet[u] = true;
+        int v;
+        for (v = 0; v < V; v++) {
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+    printMST(parent, graph);
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 2, 0, 6, 0},
+        {0, 3, 0, 0, 7},
+        {6, 8, 0, 0, 9},
+        {0, 5, 7, 9, 0}
+    };
+    primMST(graph);
+    return 0;
+}
